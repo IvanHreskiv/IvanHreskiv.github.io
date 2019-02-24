92a78053cd881fd88845220e1d94f4540d44b824
ivanhreskiv <ivanhreskiv@gmai.com>
2019-02-24 17:22:15 +0200

    Updates

---
+++ precache-manifest.cf5d40a7c37f854a3a4e047d153800e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e10b818cf00d67dc893",
+    "url": "/static/css/main.e75a508c.chunk.css"
+  },
+  {
+    "revision": "6e10b818cf00d67dc893",
+    "url": "/static/js/main.6e10b818.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0ce0c77c924b523ba0e9",
+    "url": "/static/css/2.5615eb69.chunk.css"
+  },
+  {
+    "revision": "0ce0c77c924b523ba0e9",
+    "url": "/static/js/2.0ce0c77c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "531bd43867c7f387c7a2d0a4d2dd36ba",
+    "url": "/index.html"
+  }
+];
